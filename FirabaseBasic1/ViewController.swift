792a58a30055598eae7f682d13ca02eb30c3e22b
iam <iitsukanan@yahoo.co.jp>
2019-02-16 21:26:58 +0900

    Initial Commit

---
+++ FirabaseBasic1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirabaseBasic1
+//
+//  Created by iam on 2019/02/16.
+//  Copyright © 2019 iam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
